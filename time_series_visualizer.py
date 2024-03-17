8cf481d84aa127e157a64719b6c9e44b35613f22
David Kogler <davidkogler98@gmail.com>
2024-03-17 17:00:16 +0000

    second function done

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -12,8 +12,8 @@ df = df.loc[(df['value'] > df['value'].quantile(0.025)) & (df['value'] < df['val
 
 
 def draw_line_plot():
-    # Draw line plot
 
+    # Draw line plot
     fig = plt.figure(figsize=(16, 4))
 
     plt.plot(df.index, df['value'], color='red')#, title='Daily freeCodeCamp Forum Page Views 5/2016-12/2019', xlabel='Date', ylabel='Page Views')
@@ -26,12 +26,20 @@ def draw_line_plot():
     return fig
 
 def draw_bar_plot():
+    
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+    df_bar['year'] = df_bar.index.year
+    df_bar['month'] = df_bar.index.month_name()
+    df_bar = df_bar.groupby(['year', 'month'])['value'].mean()
+    df_bar = df_bar.unstack(level='month')
+    df_bar = df_bar[['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']]
 
     # Draw bar plot
-
-
+    fig = df_bar.plot.bar(figsize=(8, 8)).figure
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    plt.legend(title='Months')
 
 
 
